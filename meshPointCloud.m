db626de21ebcf69622246e1d921e6278e3128124
kkoziol72 <33664563+kkoziol72@users.noreply.github.com>
2020-06-20 22:51:33 +0200

    Update meshPointCloud.m

--- meshPointCloud.m
+++ meshPointCloud.m
@@ -1,22 +1,42 @@
 %point cloud
 pc = pcread("data/framesTest3Limited/frameLimited1.ply");
+pc = pcdownsample(pc, 'gridAverage', 0.000001);
+
+%% final version
+
+x1 = double(pc.Location(:, 1));
+y1 = double(pc.Location(:, 2));
+z1 = double(pc.Location(:, 3));
+x = double(x1);
+y = double(y1);
+z = double(z1);
+
+tri = delaunay(x,y);
+tr = triangulation(tri, x, y, z);
+trisurf(tr);
+
+%% former version
+x1 = double(pc.Location(:, 1));
+y1 = double(pc.Location(:, 2));
+z1 = double(pc.Location(:, 3));
+P = [x1 y1 z1];
+P = alphaShape(P, 0.055);
+figure;
+plot(P);
 
 %% ver1
-
-%meshing
-
 mesh(pc.Location);
 
 %% ver2
+pc = pcdownsample(pc, 'gridAverage', 0.000001);
 
-%pc = mergedPointClouds1{1, 1};
 k = boundary(double(pc.Location(:,1)), double(pc.Location(:,2)), double(pc.Location(:,3)));
 figure;
 trisurf(k,pc.Location(:,1),pc.Location(:,2),pc.Location(:,3),'Facecolor','red','FaceAlpha',0.1)
 
 %% ver3
 
-gridStep = 0.1;
+gridStep = 0.00001;
 pc = pcdownsample(pc, 'gridAverage', gridStep);
 % delete floor
 roi = [pc.XLimits(1)-0.1 pc.XLimits(2)
@@ -27,3 +47,5 @@ pc = select(pc, indices);
 k = boundary(double(pc.Location(:,1)), double(pc.Location(:,2)), double(pc.Location(:,3)));
 figure;
 trisurf(k,pc.Location(:,1),pc.Location(:,2),pc.Location(:,3),'Facecolor','red','FaceAlpha',0.1)
+
+
